6b1f5e93c06fae74bce8d392c2d0665548a9b572
icahuang <ica.huang@qq.com>
2025-02-19 16:45:45 +0800

    Rename folders

---
+++ ass6_BVH/BVH.cpp
@@ -0,0 +1,142 @@
+#include <algorithm>
+#include <cassert>
+#include "BVH.hpp"
+
+BVHAccel::BVHAccel(std::vector<Object*> p, int maxPrimsInNode,
+                   SplitMethod splitMethod)
+    : maxPrimsInNode(std::min(255, maxPrimsInNode)), splitMethod(splitMethod),
+      primitives(std::move(p))
+{
+    time_t start, stop;
+    time(&start);
+    if (primitives.empty())
+        return;
+
+    root = recursiveBuild(primitives);
+
+    time(&stop);
+    double diff = difftime(stop, start);
+    int hrs = (int)diff / 3600;
+    int mins = ((int)diff / 60) - (hrs * 60);
+    int secs = (int)diff - (hrs * 3600) - (mins * 60);
+
+    printf(
+        "\rBVH Generation complete: \nTime Taken: %i hrs, %i mins, %i secs\n\n",
+        hrs, mins, secs);
+}
+
+// 递归生成BVH，一个node必然只指向一个object
+BVHBuildNode* BVHAccel::recursiveBuild(std::vector<Object*> objects)
+{
+    BVHBuildNode* node = new BVHBuildNode();
+
+    // // Compute bounds of all primitives in BVH node
+    // 先得到所有三角形的包围盒
+    Bounds3 bounds;
+    for (int i = 0; i < objects.size(); ++i)
+        bounds = Union(bounds, objects[i]->getBounds());
+
+    // 递归end case1: 只有一个物体时
+    if (objects.size() == 1) {
+        // Create leaf _BVHBuildNode_
+        node->bounds = objects[0]->getBounds();
+        node->object = objects[0];
+        node->left = nullptr;
+        node->right = nullptr;
+        return node;
+    }
+    // 递归end case2: 只有两个物体时
+    else if (objects.size() == 2) {
+        node->left = recursiveBuild(std::vector{objects[0]});
+        node->right = recursiveBuild(std::vector{objects[1]});
+
+        node->bounds = Union(node->left->bounds, node->right->bounds);
+        return node;
+    }
+    // 物体>=2时
+    else {
+        Bounds3 centroidBounds;
+        for (int i = 0; i < objects.size(); ++i)
+            centroidBounds =
+                Union(centroidBounds, objects[i]->getBounds().Centroid());
+        // dim = 包围盒最长的方向(0=x, 1=y, 2=z)
+        int dim = centroidBounds.maxExtent();
+        // 对objects数组做排列
+        switch (dim) {
+        // 沿着x轴做切分
+        case 0:
+            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+                return f1->getBounds().Centroid().x <
+                       f2->getBounds().Centroid().x;
+            });
+            break;
+        // 沿着y轴做切分
+        case 1:
+            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+                return f1->getBounds().Centroid().y <
+                       f2->getBounds().Centroid().y;
+            });
+            break;
+        // 沿着z轴做切分
+        case 2:
+            std::sort(objects.begin(), objects.end(), [](auto f1, auto f2) {
+                return f1->getBounds().Centroid().z <
+                       f2->getBounds().Centroid().z;
+            });
+            break;
+        }
+
+        auto beginning = objects.begin();
+        auto middling = objects.begin() + (objects.size() / 2);
+        auto ending = objects.end();
+
+        auto leftshapes = std::vector<Object*>(beginning, middling);
+        auto rightshapes = std::vector<Object*>(middling, ending);
+
+        assert(objects.size() == (leftshapes.size() + rightshapes.size()));
+
+        node->left = recursiveBuild(leftshapes);
+        node->right = recursiveBuild(rightshapes);
+
+        node->bounds = Union(node->left->bounds, node->right->bounds);
+    }
+
+    return node;
+}
+
+Intersection BVHAccel::Intersect(const Ray& ray) const
+{
+    Intersection isect;
+    if (!root)
+        return isect;
+    isect = BVHAccel::getIntersection(root, ray);
+    return isect;
+}
+
+Intersection BVHAccel::getIntersection(BVHBuildNode* node, const Ray& ray) const
+{
+    // TODO Traverse the BVH to find intersection
+
+    // std::array<int, 3> dirIsNeg[int(ray.direction.x>0),int(ray.direction.y>0),int(ray.direction.z>0)];
+    std::array<int, 3> dirIsNeg;
+    dirIsNeg[0] = ray.direction.x > 0 ? 0 : 1;
+    dirIsNeg[1] = ray.direction.y > 0 ? 0 : 1;
+    dirIsNeg[2] = ray.direction.z > 0 ? 0 : 1;
+    
+    Intersection sec;
+    if (!node->bounds.IntersectP(ray, ray.direction_inv, dirIsNeg))
+        return sec;
+    
+    // if node is a leaf node
+    if(node->left == nullptr && node->right == nullptr) {
+        // test intersection with all objs
+        // return closest intersection
+        return node->object->getIntersection(ray);
+    }
+
+    auto hit1 = getIntersection(node->left, ray);
+    auto hit2 = getIntersection(node->right, ray);
+
+    // return the closer of hit1, hit2
+    return hit1.distance < hit2.distance ? hit1 : hit2;
+}
